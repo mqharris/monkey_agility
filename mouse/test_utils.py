50b9d8f311a6eaaff8f5ef52e93dfda2a2ce338d
Mitch <mitchharris@live.ca>
2021-12-17 14:48:50 -0800

    import package name for running from main

--- mouse/test_utils.py
+++ mouse/test_utils.py
@@ -1,5 +1,5 @@
-from utils import get_angle, rotate, scale
-from pathClass import Path, get_absolute_path
+from mouse.utils import get_angle, rotate, scale
+from mouse.pathClass import Path, get_absolute_path
 import math
 
 
